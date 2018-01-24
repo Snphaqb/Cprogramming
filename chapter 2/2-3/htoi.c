42501dcbea4cb1de7f62ba2d1a6417f9cbc7002a
Snphaqb <35755098+Snphaqb@users.noreply.github.com>
2018-01-24 16:46:51 -0300

    Update htoi.c

--- chapter 2/2-3/htoi.c
+++ chapter 2/2-3/htoi.c
@@ -9,30 +9,12 @@ int pow2(int a, int b){
     return res;
 }
 int hexToDec(char c){
-    switch(c){
-	    case '0': return 0; break;
-	    case '1': return 1; break;
-	    case '2': return 2;break;
-	    case '3': return 3;break;
-	    case '4': return 4;break;
-	    case '5': return 5;break;
-	    case '6': return 6;break;
-	    case '7': return 7;break;
-	    case '8': return 8;break;
-	    case '9': return 9;break;
-	    case 'a': return 10;break;
-	    case 'b': return 11;break;
-	    case 'c': return 12;break;
-	    case 'd': return 13;break;
-	    case 'e': return 14;break;
-	    case 'f': return 15;break;
-	    case 'A': return 10;break;
-	    case 'B': return 11;break;
-	    case 'C': return 12;break;
-	    case 'D': return 13;break;
-	    case 'E': return 14;break;
-	    case 'F': return 15;break;
-    }
+    if( c >= '0' && c <= '9')
+        return c - '0';
+    if(c >= 'a' && c <= 'f')
+        return c - 'a' + 10;
+    if(c >= 'A' && c <= 'F')
+        return c - 'A' + 10;
 }
 int htoi2(char *s){
     int res = 0;	
